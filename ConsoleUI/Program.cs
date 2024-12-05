7367e1a9d7e7f8e81eca50f6d01c1e5d6c4c03c1
Emre Arslan <arslann1907@hotmail.com>
2024-12-05 20:30:12 +0100

    Ders 9 sonu

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -2,8 +2,26 @@
 using DataAccess.Concrete.EntityFramework;
 using DataAccess.Concrete.InMemory;
 
-ProductManager productManager = new(new EfProductDal());
-foreach (var product in productManager.GetByUnitPrice(40,100))
+//Data Tranformation Object = DTO
+ProductTest();
+
+//CategoryTest();
+
+
+static void ProductTest()
 {
-    Console.WriteLine(product.ProductName);
+    ProductManager productManager = new(new EfProductDal());
+    foreach (var product in productManager.GetProductDetails())
+    {
+        Console.WriteLine(product.ProductName + " / " + product.CategoryName);
+    }
+}
+
+static void CategoryTest()
+{
+    CategoryManager categoryManager = new(new EfCategoryDal());
+    foreach (var category in categoryManager.GetAll())
+    {
+        Console.WriteLine($"{category.CategoryName} = {category.CategoryId}");
+    }
 }
